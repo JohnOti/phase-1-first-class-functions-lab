0ee1968abfff2cc91824ad5bce03c2af3120252c
John Oduor Otieno <johnoduor222@gmail.com>
2022-08-16 10:17:52 -0700

    commit tree 410242ea1e172244dc25ee44c8d285244146ff33

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+const drivers = ['John', 'Otieno', 'Haryy',];
+
+const returnFirstTwoDrivers = function(drivers) {
+  return drivers.slice(0,2);
+}
+const returnLastTwoDrivers = function (drivers) {
+    return drivers.slice(-2);
+  }
+  const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+  const createFareMultiplier = function (myMultiplier) {
+    return function(value) {
+      return myMultiplier * value;
+    }
+  }
+  const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+function selectDifferentDrivers(drivers, func) {
+    return func(drivers);
+  }
